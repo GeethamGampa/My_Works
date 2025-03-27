7692a0ce6bae928c315d017588bf4afb98a6796c
Geetham Gampa <geethamgampa0905@gmail.com>
2025-03-26 21:16:50 -0400

    Printing fibonacci series

---
+++ Work_15.py
@@ -0,0 +1,11 @@
+# Printing the fibonacci series
+
+def fibonacci(n):
+    a, b = 0, 1
+    for i in range(n): 
+        print(a, end=" ")
+        a, b = b, a + b
+
+n = int(input("Enter the number : "))
+
+fibonacci(n)
